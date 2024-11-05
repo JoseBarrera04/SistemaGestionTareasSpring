40b598d0a43694101caf8518dd9f9e63572e5d2a
JoseBarrera <chapixjoestar04@gmail.com>
2024-11-05 17:17:24 -0500

    Creacion Proyecto de Spring

---
+++ src/main/java/org/radagon/sistemagestiontareas/SistemaGestionTareasApplication.java
@@ -0,0 +1,13 @@
+package org.radagon.sistemagestiontareas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGestionTareasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaGestionTareasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/radagon/sistemagestiontareas/SistemaGestionTareasApplicationTests.java
@@ -0,0 +1,13 @@
+package org.radagon.sistemagestiontareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGestionTareasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
